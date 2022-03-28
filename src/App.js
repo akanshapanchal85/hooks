60e6a0ac0c74315ab80b7c9267da2076bb222bf5
Akansha <pakansha914@gmail.com>
2022-03-28 15:58:15 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import faker from 'faker'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className='ui container comments'>
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img src={faker.image.image()} alt='avatar'/>
         </a>
-      </header>
+        <div className='content'>
+          <a href='/' className='author'>
+          Sam
+          </a> 
+        <div className='metadata'>
+          <span className='data'>Today at 6:00</span>
+        </div>
+        <div className='text'>Nice Blog Post!</div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
